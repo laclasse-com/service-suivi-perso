dea534111cc04f7f7a2866fcea7c8f2adc4c1f7c
Hadrien Quintet <hadrien.quintet@gfi.fr>
2014-09-24 16:21:32 +0200

    ajout de la variable path pour les template + modif options.sample

--- public/app/scripts/route.js
+++ public/app/scripts/route.js
@@ -1,14 +1,14 @@
 angular.module('suiviApp')
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+.config(['$stateProvider', '$urlRouterProvider', 'APP_PATH', function($stateProvider, $urlRouterProvider, APP_PATH) {
   
   $stateProvider
           .state('404', {
              url: '/404',
-             templateUrl: '/app/views/generals/404.html'
+             templateUrl: APP_PATH + '/app/views/generals/404.html'
             })
           .state( 'suivi',{
             abstract:true,
-            templateUrl:'/app/views/generals/index.html'})
+            templateUrl:APP_PATH + '/app/views/generals/index.html'})
 
           .state( 'suivi.classes',{
             resolve: { auth: function( Profil ) { Profil.redirection( ['ENS', 'DIR', 'ETA', 'EVS', 'DOC'] ); } },
@@ -16,11 +16,11 @@ angular.module('suiviApp')
             url: '/classes',
             views: {
               'aside': {
-                templateUrl:'/app/views/generals/lists/asides/aside-lists.html',
+                templateUrl:APP_PATH + '/app/views/generals/lists/asides/aside-lists.html',
                 controller: 'AsideCtrl'
               },
               'main': {
-                templateUrl:'/app/views/generals/lists/mains/classes/main-classes.html',
+                templateUrl:APP_PATH + '/app/views/generals/lists/mains/classes/main-classes.html',
                 controller: 'ClassesCtrl'
                }
               }
@@ -32,11 +32,11 @@ angular.module('suiviApp')
             url: '/classes/:classe_id/carnets',
             views: {
               'aside': {
-                templateUrl:'/app/views/generals/lists/asides/aside-lists.html',
+                templateUrl:APP_PATH + '/app/views/generals/lists/asides/aside-lists.html',
                 controller: 'AsideCtrl'
               },
               'main': {
-                templateUrl:'/app/views/generals/lists/mains/carnets/main-carnets.html',
+                templateUrl:APP_PATH + '/app/views/generals/lists/mains/carnets/main-carnets.html',
                 controller: 'CarnetsCtrl'
                }
               }
@@ -47,11 +47,11 @@ angular.module('suiviApp')
               resolve: { auth: function( Profil ) { Profil.redirection( ['ENS', 'DIR', 'ETA',] ); } },
                views: {
                    'aside': {
-                 templateUrl:'/app/views/generals/lists/asides/aside-lists.html',
+                 templateUrl:APP_PATH + '/app/views/generals/lists/asides/aside-lists.html',
                  controller: 'AsideCtrl'
                    },
                    'main': {
-                 templateUrl:'/app/views/generals/lists/mains/carnets/main-add-carnets.html',
+                 templateUrl:APP_PATH + '/app/views/generals/lists/mains/carnets/main-add-carnets.html',
                  controller: 'AddCarnetsCtrl'
                    }
                }
@@ -62,11 +62,11 @@ angular.module('suiviApp')
               resolve: { auth: function( Profil ) { Profil.redirection( ['ENS', 'DIR', 'ETA', 'EVS', 'DOC', 'ELV', 'TUT'] ); } },
                views: {
                    'aside': {
-                 templateUrl:'/app/views/generals/carnet-eleve/aside-carnet-eleve.html',
+                 templateUrl:APP_PATH + '/app/views/generals/carnet-eleve/aside-carnet-eleve.html',
                  controller: 'AsideCarnetCtrl'
                    },
                    'main': {
-                 templateUrl:'/app/views/generals/carnet-eleve/main-carnet-eleve.html',
+                 templateUrl:APP_PATH + '/app/views/generals/carnet-eleve/main-carnet-eleve.html',
                  controller: 'CarnetCtrl'
                    }
                }
@@ -77,11 +77,11 @@ angular.module('suiviApp')
                 resolve: { auth: function( Profil ) { Profil.redirection( ['ENS', 'DIR', 'ETA',] ); } },
                views: {
                    'aside': {
-                 templateUrl:'/app/views/generals/carnet-eleve/aside-carnet-eleve.html',
+                 templateUrl:APP_PATH + '/app/views/generals/carnet-eleve/aside-carnet-eleve.html',
                  controller: 'AsideCarnetCtrl'
                    },
                    'main': {
-                 templateUrl:'/app/views/generals/carnet-eleve/main-rights-carnet-eleve.html',
+                 templateUrl:APP_PATH + '/app/views/generals/carnet-eleve/main-rights-carnet-eleve.html',
                  controller: 'RightsCtrl'
                    }
                }
