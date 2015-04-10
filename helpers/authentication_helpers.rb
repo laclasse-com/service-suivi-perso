e73f25a794bc15e0711d27a1fc8cd07c1d385fdb
Pierre-Gilles Levallois <pgl@erasme.org>
2015-04-10 17:43:00 +0200

    nettoyage authHelper : tout est dans laclasse_common

--- helpers/authentication_helpers.rb
+++ helpers/authentication_helpers.rb
@@ -18,17 +18,6 @@ module AuthenticationHelpers
     $current_user ||= env['rack.session'][:current_user]
   end
 
-  # Vérifier la validité du jeton XML envoyé par CAS : du type XXX99999
-  def is_uid_valid?
-    valid = true
-    valid = false unless env['rack.session'][:current_user][:info][:uid]
-    valid = false if  env['rack.session'][:current_user][:info][:uid].match(/[a-z]{3}[0-9]{5}/i).nil?
-    valid = false unless env['rack.session'][:current_user][:info][:user]
-    valid = false unless env['rack.session'][:current_user][:info][:info].ENTPersonStructRattachRNE
-    valid = false if  env['rack.session'][:current_user][:info][:info].ENTPersonStructRattachRNE.match(/[0-9]{7}[a-z]{1}/i).nil?
-    valid
-  end
-
   # returns uri of the rack request
   def url(request)
     url = request.scheme + '://'
