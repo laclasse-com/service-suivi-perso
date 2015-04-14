1b98f4f3bbce2c28e174b0a249a76d4eccfcc0be
Pierre-Gilles Levallois <pgl@erasme.org>
2015-04-14 12:40:36 +0200

    cleanup

--- api/carnets.rb
+++ api/carnets.rb
@@ -217,33 +217,4 @@ class CarnetsApi < Grape::API
     error!('Ressource non trouvee', 404)
     end
   end
-  # before do
-  #     CarnetsLib.set_current_user get_current_user
-  # end
-
-  # desc "retourne tous les carnets d'un utilisateur E.Vignal"
-  # params {
-  #     requires :uid, type: String, desc: "uid d'un utilisateur"
-  # }
-  # get '/evignal/:uid' do
-  #     carnets = Annuaire.get_list_carnets_of(params[:uid])
-  # end
-
-  # desc "retourne tous les carnets d'un regroupement d'un utilisateur "
-  # params {
-  #   requires :uid, type: String, desc: "uid d'un utilisateur"
-  #   requires :id_rgrp, type: String, desc: "id d'un regroupement"
-  # }
-  # get '/regroupements/:uid/:id_rgrp' do
-  #   uids = Annuaire.get_carnets_regroupement_of(params[:uid], params[:id_rgrp])
-  # end
-
-  # desc "Créé un carnet dans la base de donnée"
-  # params {
-  #   # requires :uid, type: String, desc: "uid d'un utilisateur"
-  #   # requires :id_rgrp, type: String, desc: "id d'un regroupement"
-  # }
-  # get '/add/:uid' do #/:id_rgrp' do
-  #   uids = CarnetsLib.create(params[:uid])
-  # end
 end
