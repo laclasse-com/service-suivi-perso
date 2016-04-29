2616a720a8ba623a7534615b6c4fa2727c6679f9
HuguesGithub <jhugues@jhugues.fr>
2016-04-29 11:33:30 +0200

    #40 Génération listing personne - Fixed

--- lib/helpers/roles_helpers.rb
+++ lib/helpers/roles_helpers.rb
@@ -5,9 +5,13 @@ module RolesHelpers
   # trouver le role maximum sur l'étab actif
   def affecter_role_max(personnels)
     personnels.each do |personnel|
-      unless personnel['roles'].nil?
-        personnel['role_id'] = personnel['roles'].reduce( '' ) do |memo, role|
-          role['role_id'] if role['role_id'] != ROLES[:super_admin] && COEFF[ role['role_id'] ] > COEFF[ memo ]
+      next if personnel['roles'].nil?
+
+      personnel['role_id'] = personnel['roles'].reduce( '' ) do |memo, role|
+        if role['role_id'] != ROLES[:super_admin] && COEFF[ role['role_id'] ] > COEFF[ memo ]
+          role['role_id'] 
+        else
+          memo
         end
       end
     end
