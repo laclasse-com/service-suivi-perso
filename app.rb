8b41a6c93b3f2a327a9764fbc4984e427030c00f
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-08-30 14:05:35 +0200

    protocol

--- app.rb
+++ app.rb
@@ -64,7 +64,9 @@ class SinatraApp < Sinatra::Base
 
   get "#{APP_PATH}/auth/:provider/callback" do
     init_session( request.env )
-    redirect params[:url] if params[:url] != "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{APP_PATH}"
+
+    protocol = CASAUTH::CONFIG[:ssl] ? 'https://' : 'http://'
+    redirect params[:url] if params[:url] != "#{protocol}://#{env['HTTP_HOST']}#{APP_PATH}"
     redirect "#{APP_PATH}/"
   end
 
@@ -77,6 +79,7 @@ class SinatraApp < Sinatra::Base
   end
 
   get "#{APP_PATH}/logout" do
-    logout! "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{APP_PATH}"
+    protocol = CASAUTH::CONFIG[:ssl] ? 'https://' : 'http://'
+    logout! "#{protocol}://#{env['HTTP_HOST']}#{APP_PATH}"
   end
 end
