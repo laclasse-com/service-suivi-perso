982eafb718e9257e72a90a3c2e472cd11df2340f
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-03-22 09:28:38 +0100

    require gems even when called out of rack

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 # coding: utf-8
 
+require 'rubygems'
+require 'bundler'
+
+Bundler.require(:default, :development) # require tout les gems définis dans Gemfile
+require 'sinatra/reloader' if ENV['RACK_ENV'] == 'development'
+
+require_relative './config/init'
+
+require 'laclasse/cross_app/sender'
+
+require 'laclasse/helpers/authentication'
+require 'laclasse/helpers/app_infos'
+require 'laclasse/helpers/rack'
+require 'laclasse/helpers/user'
+
+require_relative './lib/init'
+require_relative './models/init'
+require_relative './objects/init'
+
 # Application Sinatra servant de base
 class SinatraApp < Sinatra::Base
   configure do
