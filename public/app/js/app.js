15d3d01840dcceab39da49c7f3c61f4b01d801de
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-03-27 11:26:26 +0200

    ng-file-upload is annoying

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,27 +2,27 @@
 
 // Declare app level module which depends on filters, and services
 angular.module( 'suiviApp',
-		[ 'ui.router',
-		  'ngResource',
-		  'ngCookies',
-		  'services.messages',
-		  'ui.bootstrap',
-		  'ui.sortable',
-		  'angular-carousel',
-		  'angular-loading-bar',
-		  'angularFileUpload',
-		  'ngClipboard',
-		  'textAngular',
-		  'growlNotifications',
-		  'nvd3ChartDirectives',
-		  'ngColorPicker'
-		] )
+                [ 'ui.router',
+                  'ngResource',
+                  'ngCookies',
+                  'services.messages',
+                  'ui.bootstrap',
+                  'ui.sortable',
+                  'angular-carousel',
+                  'angular-loading-bar',
+                  'ngFileUpload',
+                  'ngClipboard',
+                  'textAngular',
+                  'growlNotifications',
+                  'nvd3ChartDirectives',
+                  'ngColorPicker'
+                ] )
     .run( [ '$rootScope', 'Notifications', '$location', 'CurrentUser',
-	    function( $rootScope, Notifications, $location, CurrentUser ) {
-		$rootScope.$on( '$stateChangeStart',
-				function( $location ) {
-				    Notifications.clear();
-				} );
-		window.scope = $rootScope;
-		Notifications.clear();
-	    } ] );
+            function( $rootScope, Notifications, $location, CurrentUser ) {
+                $rootScope.$on( '$stateChangeStart',
+                                function( $location ) {
+                                    Notifications.clear();
+                                } );
+                window.scope = $rootScope;
+                Notifications.clear();
+            } ] );
