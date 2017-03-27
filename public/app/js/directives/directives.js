d4cdd5fecc7f3c3d0aef7ed37a84b70f558f7521
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-03-27 11:53:32 +0200

    If it's broken once it won't be good next time! fix #60

--- public/app/js/directives/directives.js
+++ public/app/js/directives/directives.js
@@ -10,26 +10,21 @@ angular.module('suiviApp')
     };
   }])
 .directive('noImage', ['LACLASSE_PATH', 'AVATAR_DEFAULT', function (LACLASSE_PATH, AVATAR_DEFAULT) {
-
     var setDefaultImage = function (el) {
         el.attr('src', LACLASSE_PATH + AVATAR_DEFAULT);
     };
 
-    return {
-        restrict: 'A',
-        link: function (scope, el, attr) {
-            scope.$watch(function() {
-                return attr.ngSrc;
-            }, function () {
-                var src = attr.ngSrc;
+    return { restrict: 'A',
+             link: function (scope, el, attr) {
+                 scope.$watch(function() {
+                     return attr.ngSrc;
+                 }, function () {
+                     var src = attr.ngSrc;
 
-                if (!src) {
-                    setDefaultImage(el);
-                }
-            });
-
-            el.bind('error', function() { setDefaultImage(el); });
-        }
-    };
+                     if (!src) {
+                         setDefaultImage(el);
+                     }
+                 });
+             }
+           };
 }]);
-
