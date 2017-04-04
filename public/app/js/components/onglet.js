9089c4e6252c38e0fa689c8c29350eefcadd1865
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-04-04 14:04:42 +0200

    saisies functional

--- public/app/js/components/onglet.js
+++ public/app/js/components/onglet.js
@@ -4,14 +4,16 @@ angular.module( 'suiviApp' )
     .component( 'onglet',
                 { bindings: { uid: '<',
                               onglet: '<' },
-                  template: 'saisies : {{$ctrl.saisies}}<ul>' +
+                  template: '<input class="btn btn-default" type="button" ng:click="$ctrl.popup_saisie( $ctrl.uid, $ctrl.onglet, null, $ctrl.$onInit )" value="+" />' +
+                  '          <ul>' +
                   '            <li ng:repeat="saisie in $ctrl.saisies">' +
                   '              <saisie uid="$ctrl.uid" onglet-id="$ctrl.onglet.id" saisie="saisie"></saisie>' +
                   '            </li>' +
                   '          </ul>',
-                  controller: [ '$uibModal', 'Onglets', 'Saisies',
-                                function( $uibModal, Onglets, Saisies ) {
+                  controller: [ '$uibModal', 'Onglets', 'Saisies', 'Popups',
+                                function( $uibModal, Onglets, Saisies, Popups ) {
                                     var ctrl = this;
+                                    ctrl.popup_saisie = Popups.saisie;
 
                                     ctrl.$onInit = function() {
                                         Saisies.query({ uid_eleve: ctrl.uid,
--- public/app/js/services/popups.js
+++ public/app/js/services/popups.js
@@ -1,7 +1,7 @@
 angular.module( 'suiviApp' )
     .service( 'Popups',
-              [ '$uibModal', 'Onglets',
-                function( $uibModal, Onglets ) {
+              [ '$uibModal', 'Onglets', 'Saisies',
+                function( $uibModal, Onglets, Saisies ) {
                     var service = this;
 
                     service.onglet = function( uid, onglet, callback ) {
@@ -76,6 +76,92 @@ angular.module( 'suiviApp' )
                                             .$delete();
                                     } else {
                                         promise = new Onglets( new_onglet )
+                                            .$update();
+                                    }
+                                }
+                                promise.then( function success() {
+                                    callback();
+                                },
+                                              function error() {} );
+                            }, function error() {  } );
+                    };
+
+                    service.saisie = function( uid, onglet, saisie, callback ) {
+                        $uibModal.open( { resolve: { saisie: function() { return _(saisie).isNull() ? { contenu: '', background_color: '#baddad' } : saisie; } },
+                                          template: '<div class="modal-header">' +
+                                          '            <h3 class="modal-title">Ajouter une saisie</h3>' +
+                                          '          </div>' +
+                                          '          <div class="modal-body available-apps">' +
+                                          '            <input type="text" ng:model="saisie.contenu" />' +
+                                          '            <div class="clearfix"></div>' +
+                                          '          </div>' +
+                                          '          <div class="modal-footer">' +
+                                          '            <button class="btn btn-danger pull-left"' +
+                                          '                    ng:click="delete()"' +
+                                          '                    ng:if="saisie.id">' +
+                                          '              <span class="glyphicon glyphicon-trash"></span>' +
+                                          '              <span> Supprimer l\'saisie</span>' +
+                                          '            </button>' +
+                                          '            <button class="btn btn-default"' +
+                                          '                    ng:click="cancel()">' +
+                                          '              <span class="glyphicon glyphicon-remove-sign"></span>' +
+                                          '              <span ng:if="saisie.contenu"> Annuler</span>' +
+                                          '              <span ng:if="!saisie.contenu"> Fermer</span>' +
+                                          '            </button>' +
+                                          '            <button class="btn btn-success"' +
+                                          '                    ng:click="ok()"' +
+                                          '                    ng:disabled="!saisie.contenu">' +
+                                          '              <span class="glyphicon glyphicon-ok-sign"></span> Valider' +
+                                          '            </button>' +
+                                          '          </div>',
+                                          controller: [ '$scope', '$uibModalInstance', 'saisie',
+                                                        function PopupSaisieCtrl( $scope, $uibModalInstance, saisie ) {
+                                                            var ctrl = $scope;
+
+                                                            ctrl.saisie = saisie;
+                                                            ctrl.saisie.delete = false;
+
+                                                            ctrl.ok = function() {
+                                                                $uibModalInstance.close( ctrl.saisie );
+                                                            };
+
+                                                            ctrl.delete = function() {
+                                                                swal({ title: 'Êtes-vous sur ?',
+                                                                       text: "La saisie sera définitivement supprimée !",
+                                                                       type: 'warning',
+                                                                       showCancelButton: true,
+                                                                       confirmButtonColor: '#3085d6',
+                                                                       confirmButtonText: 'Oui, je confirme !',
+                                                                       cancelButtonColor: '#d33',
+                                                                       cancelButtonText: 'Annuler'
+                                                                     })
+                                                                    .then( function() {
+                                                                        ctrl.saisie.delete = true;
+                                                                        ctrl.ok();
+                                                                    } );
+                                                            };
+
+                                                            ctrl.cancel = function() {
+                                                                $uibModalInstance.dismiss();
+                                                            };
+                                                        } ] } )
+                            .result.then( function success( new_saisie ) {
+                                var promise = null;
+                                if ( _(saisie).isNull() ) {
+                                    promise = new Saisies({ uid_eleve: uid,
+                                                            onglet_id: onglet.id,
+                                                            contenu: new_saisie.contenu,
+                                                            background_color: new_saisie.background_color })
+                                        .$save();
+                                } else {
+                                    new_saisie.uid_eleve = uid;
+                                    new_saisie.onglet_id = onglet.id;
+
+                                    if ( new_saisie.delete ) {
+                                        promise = new Saisies( new_saisie )
+                                            .$delete();
+                                    } else {
+                                        promise = new Saisies( new_saisie )
                                             .$update();
                                     }
                                 }
