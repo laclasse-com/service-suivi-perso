e9e48fc2abfb8d12b44df9164cea89cbed97f67c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-04-19 14:44:01 +0200

    drop constants kludge

--- config/constants.rb
+++ config/constants.rb
@@ -5,27 +5,6 @@ URL_REQUEST_TEST = ''.freeze
 
 LOGGER = Logger.new(STDOUT)
 
-COLOR = { rouge: '#EB5454',
-          bleu: '#1AA1CC',
-          violet: '#9C75AB',
-          jaune: '#E8C254',
-          vert: '#80BA66' }.freeze
-
-ANNUAIRE_URL = { suivi_perso_search: 'eleves/search/',
-                 user_liste: 'liste/' }.freeze
-
-# Couleurs des carnets sur 16 cases
-PANEL_COLOR = [ COLOR[:bleu], COLOR[:jaune], COLOR[:violet], COLOR[:vert],
-                COLOR[:rouge], COLOR[:vert], COLOR[:bleu], COLOR[:jaune],
-                COLOR[:violet], COLOR[:bleu], COLOR[:jaune], COLOR[:rouge],
-                COLOR[:jaune], COLOR[:rouge], COLOR[:vert], COLOR[:bleu] ].freeze
-
-AVATAR = { F: '/app/node_modules/laclasse-common-client/images/avatar_feminin.svg',
-           M: '/app/node_modules/laclasse-common-client/images/avatar_masculin.svg' }.freeze
-
-UAI_EVIGNAL = '0692165D'.freeze
-MAIL_DOMAINE = '@laclasse.com'.freeze
-
 # Gestion des droits
 DEFAULT_RIGHTS = { Carnet: [ { profil_id: 'ENS', read: true, write: true },
                              { profil_id: 'EVS', read: true, write: true },
