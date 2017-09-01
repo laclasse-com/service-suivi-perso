569ce6167347e76fce41de4fe5250029a173b02d
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-09-01 12:00:22 +0200

    fix colors, fix #70

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -29,8 +29,10 @@ angular.module( 'suiviApp',
                                            $delegate.editLink.targetToggle.buttontext = 'Le lien s\'ouvrira dans une nouvelle fenêtre';
                                            $delegate.editLink.reLinkButton.tooltip = 'Éditer le lien';
                                            $delegate.editLink.unLinkButton.tooltip = 'Enlever le lien';
+                                           $delegate.insertImage.tooltip = 'Insérer une image';
+                                           $delegate.insertImage.dialogPrompt = 'URL de l\'image :';
                                            $delegate.insertVideo.tooltip = 'Insérer une vidéo';
-                                           //$delegate.insertVideo.dialogPrompt = 'Skriv Youtube video URL, der skal indsættes';
+                                           $delegate.insertVideo.dialogPrompt = 'URL de la vidéo Youtube :';
                                            $delegate.clear.tooltip = 'Enlever le formattage';
                                            $delegate.ul.tooltip = 'Liste';
                                            $delegate.ol.tooltip = 'Liste numérotée';
@@ -44,58 +46,25 @@ angular.module( 'suiviApp',
                    // configuration de textAngular
                    $provide.decorator( 'taOptions',
                                        [ '$delegate', 'taRegisterTool',
-                                         function( taOptions, taRegisterTool ) {
-                                             var colorpicker_taTool = function( attribute, tooltiptext ) {
+                                         function( taOptions, taRegisterTool ){
+                                             var colorpicker_taTool = function( type ) {
+                                                 var style_prefix = ( type === 'backcolor' ) ? 'background-' : '';
                                                  var couleurs = [ '#7bd148', '#5484ed', '#a4bdfc', '#46d6db', '#7ae7bf', '#51b749', '#fbd75b', '#ffb878', '#ff887c', '#dc2127', '#dbadff', '#e1e1e1' ];
-                                                 if ( attribute === 'background-color' ) {
+                                                 if ( type === 'backcolor' ) {
                                                      couleurs.push( 'transparent' );
                                                  }
 
                                                  return { couleurs: couleurs,
-                                                          display: '<span uib-dropdown><a uib-dropdown-toggle><i class="fa fa-font" data-ng-style="{\'' + attribute + '\': selected }"></i> <i class="fa fa-caret-down"></i></a><ng-color-picker uib-dropdown-menu selected="selected" colors="couleurs"></ng-color-picker></span>',
-                                                          tooltiptext: tooltiptext,
+                                                          display: '<span uib-dropdown><a uib-dropdown-toggle><i class="fa fa-font" data-ng-style="{\'' + style_prefix + 'color\': selected }"></i> <i class="fa fa-caret-down"></i></a><ng-color-picker uib-dropdown-menu selected="selected" colors="couleurs"></ng-color-picker></span>',
                                                           action: function( ) {
-                                                              return ( this.selected === 'nil' ) ? false : this.$editor().wrapSelection( attribute, this.selected );
+                                                              return ( this.selected === 'nil' ) ? false : this.$editor().wrapSelection( type, this.selected );
                                                           }
                                                         };
                                              };
 
-                                             taRegisterTool( 'fontColor', colorpicker_taTool( 'color' ), 'Changer la couleur du texte' );
+                                             taRegisterTool( 'fontColor', colorpicker_taTool( 'forecolor' ) );
 
-                                             // taRegisterTool( 'backgroundColor', colorpicker_taTool( 'background-color' ), 'Changer la couleur de fonds' );
-
-                                             taRegisterTool( 'table', { columns: { value: 1,
-                                                                                   hovered: 1 },
-                                                                        rows: { value: 1,
-                                                                                hovered: 1 },
-                                                                        hover: function( objet, value ) {
-                                                                            objet.hovered = value;
-                                                                        },
-                                                                        leave: function( objet ) {
-                                                                            objet.hovered = objet.value;
-                                                                        },
-                                                                        tooltiptext: 'insérer un tableau',
-                                                                        display: '<span uib-dropdown><a uib-dropdown-toggle><i class="fa fa-table"></i> <i class="fa fa-caret-down"></i></a><div uib-dropdown-menu data-ng-click="$event.stopPropagation()"><label><rating on-hover="hover( columns, value )" on-leave="leave( columns )" ng-model="columns.value" max="15" state-on="\'glyphicon-stop\'" state-off="\'glyphicon-unchecked\'"></rating><br>{{columns.hovered}} colonnes</label><br><label><rating on-hover="hover( rows, value )" on-leave="leave( rows )" ng-model="rows.value" max="15" state-on="\'glyphicon-stop\'" state-off="\'glyphicon-unchecked\'"></rating><br>{{rows.hovered}} lignes</label><br><button class="btn btn-success" data-ng-click="insert_table()">Insérer</button></div></span>',
-                                                                        insert_table: function(  ) {
-                                                                            var tds = '';
-                                                                            for ( var idxCol = 0; idxCol < this.columns.value; idxCol++ ) {
-                                                                                tds = tds + '<td>&nbsp;</td>';
-                                                                            }
-                                                                            var trs = '';
-                                                                            for ( var idxRow = 0; idxRow < this.rows.value; idxRow++ ) {
-                                                                                trs = trs + '<tr>'+ tds + '</tr>';
-                                                                            }
-
-                                                                            this.$editor().wrapSelection( 'insertHTML', '<table class="table table-bordered">' + trs + '</table>' );
-
-                                                                            this.deferration.resolve();
-                                                                        },
-                                                                        action: function( deferred  ) {
-                                                                            this.deferration = deferred;
-                                                                            return false;
-                                                                        } } );
-
-                                             taOptions.toolbar = [ [ 'bold', 'italics', 'underline', 'ul', 'ol', 'quote', 'fontColor', 'table', 'justifyLeft', 'justifyCenter', 'justifyRight', 'insertLink', 'html', 'undo', 'redo', 'charcount' ] ];
+                                             taOptions.toolbar = [ [ 'bold', 'italics', 'underline', 'ul', 'ol', 'quote', 'fontColor', 'justifyLeft', 'justifyCenter', 'justifyRight', 'insertLink', 'insertImage', 'insertVideo', 'html', 'undo', 'redo' ] ];
 
                                              taOptions.classes = {
                                                  focussed: 'focussed',
@@ -107,7 +76,6 @@ angular.module( 'suiviApp',
                                                  textEditor: 'form-control',
                                                  htmlEditor: 'form-control'
                                              };
-
                                              return taOptions;
                                          } ] );
 
