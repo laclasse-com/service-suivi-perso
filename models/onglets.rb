c4e3b4b3729c3d6a497ba4ed7e219c026749fd86
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-09-26 11:20:12 +0200

    élève has RW on new onglets by default

--- models/onglets.rb
+++ models/onglets.rb
@@ -15,6 +15,7 @@ class Onglet < Sequel::Model(:onglets)
   def init_droits( default_rights, user_creator )
     add_droit( uid: user_creator['id'], profil_id: nil, sharable_id: nil, read: true, write: true, manage: true )
     return if carnets.uid_eleve == user_creator['id']
+    add_droit( uid: carnets.uid_eleve, profil_id: nil, sharable_id: nil, read: true, write: true, manage: false )
     default_rights.each do |default_right|
       add_droit( default_right )
     end
