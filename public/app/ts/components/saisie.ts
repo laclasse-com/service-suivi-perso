65864822248d1919f4049abcd7d5add6faedc072
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-12-12 16:30:48 +0100

    typescript

--- public/app/ts/components/saisie.ts
+++ public/app/ts/components/saisie.ts
@@ -51,7 +51,6 @@ angular.module('suiviApp')
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             confirmButtonText: 'Oui, je confirme !',
-            cancelButtonColor: '#d33',
             cancelButtonText: 'Annuler'
           })
             .then(function() {
@@ -90,9 +89,9 @@ angular.module('suiviApp')
           ctrl.saisie.trusted_contenu = $sce.trustAsHtml(ctrl.saisie.contenu);
         };
       }],
-                 template: `
-                 <div class="panel panel-default saisie-display" ng:class="{'new-saisie': $ctrl.new_saisie}">
-                   <span style="position: absolute; top: 0; right: 15px;height: 0;width: 0;text-align: center; color: #fff; border-color: transparent #fa0 transparent transparent;border-style: solid;border-width: 0 50px 50px 0; z-index: 1;"
+    template: `
+<div class="panel panel-default saisie-display" ng:class="{'new-saisie': $ctrl.new_saisie}">
+<span style="position: absolute; top: 0; right: 15px;height: 0;width: 0;text-align: center; color: #fff; border-color: transparent #fa0 transparent transparent;border-style: solid;border-width: 0 50px 50px 0; z-index: 1;"
                          ng:if="$ctrl.saisie.tmp_pinned">
                      <span class="glyphicon glyphicon-pushpin" style="margin-left: 25px;font-size: 22px;margin-top: 3px;"></span>
                    </span>
