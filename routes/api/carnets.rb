444007a645aceb0371a109376ea2c69fc779a439
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2018-02-15 11:18:16 +0100

    used only by current user

--- routes/api/carnets.rb
+++ routes/api/carnets.rb
@@ -4,7 +4,7 @@ module Suivi
       module Carnets
         def self.registered( app )
           app.get '/api/carnets/relevant/:uid' do
-            # halt( 401, '401' ) if params['uid'] != user['id']
+            halt( 401, '401' ) if params['uid'] != user['id']
 
             json( Carnet.where( id: Onglet.where( id: Saisie.where( uid_author: params['uid'] )
                                                             .association_join( :onglets )
